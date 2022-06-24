ec1ce336b651d5efbc83fcd171864f07011fc0dd
joe-white09 <joe_white09@yahoo.com>
2022-06-24 13:38:15 -0500

    separated employee question from other objects

--- index.js
+++ index.js
@@ -1,69 +1,103 @@
 const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
 
 const { writeFile, copyFile } = require('./src/generate-site');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
-const promptManager = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of your Team Manager? ',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the Team Manager's name!");
-                    return false;
-                }
+const employeeQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the employee? ',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+             } else {
+               console.log("Please enter the employee's name!");
+                return false;
             }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Please enter the employee id number: ',
-            validate: idInput => {
-                if (idInput) {
-                    return true;
-                } else {
-                    console.log('You must enter an employee id number!');
-                    return false;
-                }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Please enter the employee id number: ',
+        validate: idInput => {
+            if (idInput) {
+                 return true;
+            } else {
+                console.log('You must enter an employee id number!');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Please enter the Team Manager's email address: ",
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('You must enter an email address for the Team Manager!');
-                    return false;
-                }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter the employee's email address: ",
+        validate: emailInput => {
+            if (emailInput) {
+               return true;
+            } else {
+                console.log('You must enter an email address for the employee!');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: "What is the Team Manager's office phone number? ",
-            validate: officeNumberInput => {
-                if (officeNumberInput) {
-                    return true;
-                } else {
-                    console.log('You must enter the office phone number!');
-                    return false;
-                }
+        }
+    },
+];
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the Team Manager's office phone number? ",
+        validate: officeNumberInput => {
+            if (officeNumberInput) {
+                return true;
+            } else {
+                console.log('You must enter the office phone number!');
+                return false;
             }
-        },
-    ])
+        }
+    },
+];
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: "Please enter the Engineer's GitHub username?",
+        validate: githubInput => {
+            if(githubInput) {
+                return true;
+            } else {
+                console.log('You must enter a GitHub username!');
+                return false;
+            }
+        }
+    }
+];
+
+const buildManager = () => {
+    console.log('Please enter information for the Team Manager')
+    return inquirer.prompt(employeeQuestions.concat(managerQuestions))
     .then(({name, id, email, officeNumber }) => {
         this.manager = new Manager(name, id, email, officeNumber);
         console.log(this.manager);
-    })
+    });
 };
 
-promptManager();
+const buildEngineer = () => {
+    return inquirer.prompt(employeeQuestions.concat(engineerQuestions))
+    .then(({name, id, email, github }) => {
+        this.engineer = new Engineer(name, id, email, github);
+        console.log(this.engineer);
+    })
+}
+
+buildManager()
+    .then(buildEngineer);
+
+
     
