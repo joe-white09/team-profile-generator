0f6ba975dd14a887ae9a1d9eee51f7262bb8df09
joe-white09 <joe_white09@yahoo.com>
2022-06-25 17:55:51 -0500

    added boostrap styles to touch up html display

--- src/page-template.js
+++ src/page-template.js
@@ -2,42 +2,65 @@ const generateManager = managerArr => {
     if(managerArr.length === 0) {
         return '';
     }
-    return `
-        <div>      
+    return `      
         ${managerArr.map(({ name, id, email, officeNumber, role }) => {
               return `
-            <div>
-                <h3>${name}</h3>
-                <h4>${role}</h4>
-                    <p>ID: ${id}</p>
-                    <p>Email: <a href="mailto:${email}">${email}</a></p>
-                    <p>Office Number: ${officeNumber}</p>
-            
+            <div class="card m-2 p-2 shadow row col-md-4 col-lg-3">
+              <div class="card-title bg-primary text-white">
+                  <h3 class="m-2">${name}</h3>
+                  <h4><i class="fa-solid fa-mug-hot p-2"></i>${role}</h4>
+              </div>
+                  <p class="card-text border p-2">ID: ${id}</p>
+                  <p class="card-text border p-2">Email: <a href="mailto:${email}">${email}</a></p>
+                  <p class="card-text border p-2">Office Number: ${officeNumber}</p>
+            </div>          
             `;
             })
-            }</div>`
+            }`
 };
 
 const generateEngineer = engineerArr => {
     if(engineerArr.length === 0) {
         return '';
     }
-    return `
-    <div>      
+    return `     
     ${engineerArr.map(({ name, id, email, github, role }) => {
           return `
-          <div>
-            <div>
-            <h3>${name}</h3>
-            <h4>${role}</h4>
-                <p>ID: ${id}</p>
-                <p>Email: <a href="mailto:${email}">${email}</a></p>
-                <p>Github: <a href="https://github.com/${github}/" target="_blank">${github}</a></p>
-            </div>
+        <div class="card m-2 p-2 shadow row col-md-4 col-lg-3">
+          <div class="card-title bg-primary text-white">
+              <h3 class="m-2">${name}</h3>
+              <h4><i class="fa-solid fa-glasses p-2"></i>${role}</h4>
+          </div>
+              <p class="card-text border p-2">ID: ${id}</p>
+              <p class="card-text border p-2">Email: <a href="mailto:${email}">${email}</a></p>
+              <p class="card-text border p-2">GitHub: ${github}</p>
+        </div>      
         `;
         })
         .join('')}
-        </div>`
+        `
+};
+
+const generateIntern = internArr => {
+    if(internArr.length === 0) {
+        return '';
+    }
+    return `          
+    ${internArr.map(({ name, id, email, school, role }) => {
+          return `
+        <div class="card m-2 p-2 shadow row col-md-4 col-lg-3">
+            <div class="card-title bg-primary text-white">
+                <h3 class="m-2">${name}</h3>
+                <h4><i class="fa-solid fa-user-graduate p-2"></i>${role}</h4>
+            </div>
+                <p class="card-text border p-2">ID: ${id}</p>
+                <p class="card-text border p-2">Email: <a href="mailto:${email}">${email}</a></p>
+                <p class="card-text border p-2">School: ${school}</p>
+        </div>
+        `;
+        })
+        .join('')}
+        `
 };
 
 const generatePage = teamArr => {
@@ -50,9 +73,6 @@ const generatePage = teamArr => {
     var internArr = teamArr.filter(function(teamArr) {
         return teamArr.role === 'Intern';
     });
-    console.log(managerArr);
-    console.log(engineerArr);
-    console.log(internArr);
     return `
   <!DOCTYPE html>
   <html lang="en">
@@ -61,8 +81,10 @@ const generatePage = teamArr => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Team Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/04d8a877e6.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
+    <title>Team Profile</title>
   </head>
 
   <body>
@@ -70,13 +92,13 @@ const generatePage = teamArr => {
       <h1>My Team</h1>
     </header>
     <main>
+        <div class= " d-flex flex-wrap justify-content-center">
           ${generateManager(managerArr)}
           ${generateEngineer(engineerArr)}
+          ${generateIntern(internArr)}
+        </div>
 
     </main>
-    <footer >
-      
-    </footer>
   </body>
   </html>
   `;
